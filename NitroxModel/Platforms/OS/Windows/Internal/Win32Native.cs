b83d979cd99f43e424b3c74b7e6ba423711f719f
paschoarthur <paschoalinarthur07@gmail.com>
2025-11-21 13:37:36 -0300

    Update Win32Native.cs

--- NitroxModel/Platforms/OS/Windows/Internal/Win32Native.cs
+++ NitroxModel/Platforms/OS/Windows/Internal/Win32Native.cs
@@ -98,7 +98,7 @@ internal static class Win32Native
 
     public static bool IsTrusted(string fileName)
     {
-        return WinVerifyTrust(fileName) == 0;
+        return WinVerifyTrust(fileName) == true;
     }
 
     private static uint WinVerifyTrust(string fileName)
